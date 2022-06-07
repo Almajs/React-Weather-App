94e70681f81e0d3e0add26c213405749f8fb04af
AlmaJS <alma.bocaj@gmail.com>
2022-06-07 17:56:16 +0100

    updated the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="mailto:alma.bocaj@gmail.com">Alma Zamir </a> and is {}
+        <a href=" https://github.com/Almajs/react-weather-app" target="blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
