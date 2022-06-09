df57969227dfcdd97d6776d28eece307efea3cdd
AlmaJS <alma.bocaj@gmail.com>
2022-06-09 15:16:10 +0100

    imported axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a href="mailto:alma.bocaj@gmail.com">Alma Zamir </a> and is {}
-        <a href=" https://github.com/Almajs/react-weather-app" target="blank">
-          open sourced on GitHub
-        </a>
-      </footer>
-    </div>
-  );
+import "./App.css";
+import axios from "axios";
+
+export default function App(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} degrees`
+    );
+  }
+  let apiKey = "6c9652fb4aafdb6667f90afa73f1d29b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
 }
+
+// <div className="App">
+//   <h1> Weather App</h1>
+//   <footer>
+//     {" "}
+//     This project was coded by{" "}
+//     <a href="mailto:alma.bocaj@gmail.com">Alma Zamir </a> and is {}
+//     <a href=" https://github.com/Almajs/react-weather-app" target="blank">
+//       open sourced on GitHub
+//     </a>
+//   </footer>
+// </div>
+// );
