b2850a684bc065219d2685088d0b52363a3a692e
AlmaJS <alma.bocaj@gmail.com>
2022-06-09 15:55:53 +0100

    imported react-loader-spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from "./logo.svg";
 import React from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { TailSpin } from "react-loader-spinner";
 
 import "./App.css";
 import axios from "axios";
@@ -7,13 +9,16 @@ import axios from "axios";
 export default function App(props) {
   function handleResponse(response) {
     alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp} degrees`
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )} degrees`
     );
   }
   let apiKey = "6c9652fb4aafdb6667f90afa73f1d29b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
+  return <TailSpin height="100" width="100" color="grey" ariaLabel="loading" />;
 }
 
 // <div className="App">
