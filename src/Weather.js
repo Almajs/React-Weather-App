e9b96256f5bd57a3a846768c7372bd5baea1b063
AlmaJS <alma.bocaj@gmail.com>
2022-06-14 13:40:17 +0100

    added the date in the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
-import App from "./App";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -10,13 +10,13 @@ export default function Weather(props) {
 
     setWeatherData({
       ready: true,
+      city: response.data.name,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
       wind: response.data.wind.speed,
-      city: response.data.main.name,
     });
   }
 
@@ -44,7 +44,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
@@ -65,8 +67,8 @@ export default function Weather(props) {
           </div>
           <div className="col-6">
             <ul>
-              <li>{weatherData.humidity} %</li>
-              <li>{weatherData.wind} km/h</li>
+              <li>Humidity: {Math.round(weatherData.humidity)} %</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
             </ul>
           </div>
         </div>
