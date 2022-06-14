cd2e0504e232e22224e718c335a4214f8718bc56
AlmaJS <alma.bocaj@gmail.com>
2022-06-14 16:50:03 +0100

    added the Weather icon component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,20 +15,20 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt="mostly cloudy"
-              className="float-left"
-            ></img>
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+            <div className="col-6">
+              <div className="float-left">
+                <span className="temperature">
+                  {Math.round(props.data.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-2">
           <ul>
             <li>Humidity: {Math.round(props.data.humidity)} %</li>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
