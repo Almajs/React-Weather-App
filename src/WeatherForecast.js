660fc11c8c31a62fcee69407734ad29d0a4458ce
AlmaJS <alma.bocaj@gmail.com>
2022-06-22 15:38:03 +0100

    updated WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,26 +20,16 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <div className="row">
-              <div className="col-2">
-                <WeatherForecastDay data={forecast[0]} />
-              </div>
-              <div className="col-2">
-                {" "}
-                <WeatherForecastDay data={forecast[1]} />
-              </div>
-              <div className="col-2">
-                <WeatherForecastDay data={forecast[2]} />
-              </div>
-              <div className="col-2">
-                <WeatherForecastDay data={forecast[3]} />
-              </div>
-              <div className="col-2">
-                <WeatherForecastDay data={forecast[4]} />
-              </div>
-            </div>
-          </div>
+          {forecast.map(function(dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+          ;
         </div>
       </div>
     );
